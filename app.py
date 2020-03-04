853293cc707984474aa654a79fc26a57bb9cfcd1
Corey <coreyjake.hansen@gmail.com>
2020-03-04 12:57:59 -0800

    request and scrape Steam for price & added some app structure

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
-from learning import learning_blueprint
+import re
+import requests
+from bs4 import BeautifulSoup
 
-app = Flask(__name__)
+URL = "https://store.steampowered.com/app/653530/Return_of_the_Obra_Dinn/"
+TAG = "div"
+QUERY = {"class": "game_purchase_price price"}
 
-app.register_blueprint(learning_blueprint, url_prefix='/greet')
+response = requests.get("https://store.steampowered.com/app/653530/Return_of_the_Obra_Dinn/")
+content = response.content
+soup = BeautifulSoup(content, "html.parser")
+element = soup.find(TAG, QUERY)
+string_price = element.text.strip()
 
-if __name__ == "__main__":
-    app.run()
+pattern = re.compile(r"(\d+\.\d\d)")
+match = pattern.search(string_price)
+found_price = match.group(1)
+price = float(found_price)
+
+print(price)
