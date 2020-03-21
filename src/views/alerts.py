2139427147bc205e035d784fd8efe608c1dda883
Corey <coreyjake.hansen@gmail.com>
2020-03-20 18:27:20 -0700

    add alerts from the item card in '/items/all' tried to add boostrap collapse items so that the New Alert form was hidden inside item card

--- src/views/alerts.py
+++ src/views/alerts.py
@@ -1,18 +1,21 @@
 import json
-from flask import Blueprint, request, render_template
+from flask import Blueprint, redirect, request, render_template, url_for
 from models.item import Item
 from models.alert import Alert
 
 alert_blueprint = Blueprint('alerts', __name__)
 
-@alert_blueprint.route('/new', methods=['GET', 'POST'])
-def new_alert():
-    if request.method == 'POST':
-        item_id = request.form['item_id']
-        price_limit = request.form['price_limit']
-        if Item.get_by_id(item_id) is not None:
+@alert_blueprint.route('/new/<string:item_id>', methods=['GET', 'POST'])
+def new_alert(item_id):
+    if Item.get_by_id(item_id) is not None:
+        if request.method == 'POST':
+            price_limit = request.form['price_limit']
             Alert(item_id, price_limit).save_to_mongo()
-    return render_template('alerts/new_alert.html')
+            return redirect(url_for('alerts.all_alerts'))
+        item = Item.get_by_id(item_id)
+        return render_template('alerts/new_alert.html', item=item)
+    else:
+        return render_template('items/all_items.html')
 
 @alert_blueprint.route('/all', methods=['GET'])
 def all_alerts():
