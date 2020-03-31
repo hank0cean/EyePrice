cbcf4c63dbcc82ac7c965a632941b7da78d2aa2f
Corey <coreyjake.hansen@gmail.com>
2020-03-30 17:24:11 -0700

    edit/delete stores & alerts

--- src/views/alerts.py
+++ src/views/alerts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
 from models.item import Item
 from models.alert import Alert
 from models.store import Store
@@ -16,8 +16,29 @@ def new_alert():
         item.load_price()
         item.save_to_mongo()
         Alert(item._id, price_limit).save_to_mongo()
+        return redirect(url_for('.all_alerts'))
     return render_template('alerts/new_alert.html')
 
+@alert_blueprint.route('/edit', methods=['GET'])
+@alert_blueprint.route('/edit/<string:alert_id>', methods=['GET', 'POST'])
+def edit_alert(alert_id):
+    alert = Alert.get_by_id(alert_id)
+    if alert is not None:
+        if request.method == 'POST':
+            new_price_limit = float(request.form['price_limit'])
+            alert.price_limit = new_price_limit
+            alert.save_to_mongo()
+            return redirect(url_for('.all_alerts'))
+        return render_template('alerts/edit_alert.html', alert=alert)
+    else:
+        return redirect(url_for('.all_alerts'))
+
+@alert_blueprint.route('/delete/<string:alert_id>')
+def delete_alert(alert_id):
+    Alert.get_by_id(alert_id).remove_from_mongo()
+    return redirect(url_for('.all_alerts'))
+
+@alert_blueprint.route('/', methods=['GET'])
 @alert_blueprint.route('/all', methods=['GET'])
 def all_alerts():
     alerts = Alert.all()
--- src/views/stores.py
+++ src/views/stores.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, redirect, render_template, request, url_for
 import json
 
 from models.store import Store
@@ -15,6 +15,28 @@ def new_store():
         if store:
             store.save_to_mongo()
     return render_template('stores/new_store.html')
+
+@store_blueprint.route('/edit/<string:store_id>', methods=['GET', 'POST'])
+def edit_store(store_id):
+    store = Store.get_by_id(store_id)
+    if store is not None:
+        if request.method == 'POST':
+            new_url_prefix = request.form['url_prefix']
+            new_tag_name = request.form['tag_name']
+            new_query = request.form['query']
+            store.url_prefix = new_url_prefix
+            store.tag_name = new_tag_name
+            store.query = new_query
+            store.save_to_mongo()
+            return redirect(url_for('.all_stores'))
+        return render_template('stores/edit_store.html', store=store)
+    else:
+        return redirect(url_for('.all_stores'))
+
+@store_blueprint.route('/delete/<string:store_id>')
+def delete_store(store_id):
+    Store.get_by_id(store_id).remove_from_mongo()
+    return redirect(url_for('.all_stores'))
 
 @store_blueprint.route('/', methods=['GET'])
 @store_blueprint.route('/all', methods=['GET'])
